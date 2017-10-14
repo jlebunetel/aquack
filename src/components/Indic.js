4d33d2153c061d4ea1870492cab588c0f54ecd58
katoyuji <kato.yuji@gmail.com>
2017-10-14 19:13:16 +0200

    boutons d'indicateurs
    toggle buttons pour les indicateurs

--- src/components/Indic.js
+++ src/components/Indic.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
-
+import { Grid, Col, Jumbotron, Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import Carte from "./Carte";
 
 export default class Indic extends Component {
 
 	render() {
-
 		return (
 			<div>
 				<Jumbotron>
 					<h1>Ajustez les indicateurs</h1>
 				</Jumbotron>
-				<br/>
+				
+				<div>
+					<Grid>
+						<Col md="4">
+							<h1>Milieu Physique</h1>
+							<ToggleButtonGroup name="physic" type="checkbox">
+								<ToggleButton value={1} bsStyle="primary">Bathymétrie</ToggleButton>
+								<ToggleButton value={2} bsStyle="primary">Courants</ToggleButton>
+								<ToggleButton value={3} bsStyle="primary">Hauteur de vagues</ToggleButton>
+							</ToggleButtonGroup>
+						</Col>
+						<Col md="4">
+							<h1>Milieu Naturel</h1>
+							<ToggleButtonGroup name="natural" type="checkbox">
+								<ToggleButton value={1}>Espaces sensibles</ToggleButton>
+								<ToggleButton value={2}>Qualité du milieu</ToggleButton>
+							</ToggleButtonGroup>
+						</Col>
+						<Col md="4">
+							<h1>Usages</h1>
+							<ToggleButtonGroup name="physic" type="checkbox">
+								<ToggleButton value={1} bsStyle="warning">Zones militaires</ToggleButton>
+								<ToggleButton value={2} bsStyle="warning">Pêche</ToggleButton>
+								<ToggleButton value={3} bsStyle="warning">Trafic maritime</ToggleButton>
+							</ToggleButtonGroup>
+						</Col>
+					</Grid>
+				</div>
+				<div>
+					<Grid>
+						<Col md="12">
+							<br />
+						</Col>
+					</Grid>
+				</div>
 				<Carte />
 			</div>
-		)
-		
+			)
 	}
 }
